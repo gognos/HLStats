0867910625ebfba20944a0ef65ae9e841c1eaa1d
jumpin-banana <jumpin.banana@gmail.com>
2010-07-08 14:35:38 +0200

    admin weapons manage page

--- web/hlstatsinc/admintasks/weapons.inc.php
+++ web/hlstatsinc/admintasks/weapons.inc.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * manage the weapons
+ * @package HLStats
+ * @author Johannes 'Banana' Keßler
+ * @copyright Johannes 'Banana' Keßler
+ */
+
+
+/**
  *
  * Original development:
  * +
@@ -39,50 +47,134 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-	if ($auth->userdata["acclevel"] < 80) die ("Access denied!");
+$gc = false;
+$check = false;
+$return = false;
 
-	$edlist = new EditList("weaponId", DB_PREFIX."_Weapons", "gun");
-	$edlist->columns[] = new EditListColumn("game", "Game", 0, true, "hidden", $gamecode);
-	$edlist->columns[] = new EditListColumn("code", "Weapon Code", 15, true, "text", "", 32);
-	$edlist->columns[] = new EditListColumn("name", "Weapon Name", 25, true, "text", "", 64);
-	$edlist->columns[] = new EditListColumn("modifier", "Points Modifier", 10, true, "text", "1.00");
-
-
-	if ($_POST)
-	{
-		if ($edlist->update())
-			message("success", l("Operation successful"));
-		else
-			message("warning", $edlist->error());
+// get the game, without it we can no do anyting
+if(isset($_GET['gc'])) {
+	$gc = trim($_GET['gc']);
+	$check = validateInput($gc,'nospace');
+	if($check === true) {
+		// load the server
+		$query = mysql_query("SELECT s.serverId, s.address, s.port,
+								s.name AS serverName,
+								s.publicaddress, s.statusurl,
+								s.rcon_password, s.defaultMap,
+								g.name AS gameName
+							FROM `".DB_PREFIX."_Servers` AS s
+							LEFT JOIN `".DB_PREFIX."_Games` AS g ON g.code = s.game
+							WHERE s.game = '".mysql_escape_string($gc)."'
+							ORDER BY address ASC, port ASC");
+		if(mysql_num_rows($query) > 0) {
+			while($result = mysql_fetch_assoc($query)) {
+				$servers[] = $result;
+			}
+		}
 	}
+	mysql_free_result($query);
+}
+
+// do we have a valid gc code?
+if(empty($gc) || empty($check)) {
+	exit('No game code given');
+}
+
+$weapons = false;
+// get the teams
+$query = mysql_query("SELECT weaponId, code, name, modifier
+						FROM `".DB_PREFIX."_Weapons`
+						WHERE game='".mysql_escape_string($gc)."'
+						ORDER BY code ASC");
+if(mysql_num_rows($query) > 0) {
+	while($result = mysql_fetch_assoc($query)) {
+		$weapons[] = $result;
+	}
+}
+
+$rcol = "row-dark";
+
+pageHeader(array(l("Admin"),l('Weapons')), array(l("Admin")=>"index.php?mode=admin",l('Weapons')=>''));
 ?>
 
-<?php echo l('You can give each weapon a "points modifier", amultiplier which determines how many points will be gained or lost for killing with or being killed by that weapon'); ?>.
-<?php echo l('(Refer to'), ' <a href="index.php?mode=help#points">', l('Help'),'</a>' ?>
-<?php echo l("for a full description of how points ratings are calculated.) The baseline points modifier for weapons is 1.00. A points modifier of 0.00 will cause kills with that weapon to have no effect on players' points"); ?>.<p>
-
-<?php
-
-
-	$query = mysql_query("
-		SELECT
-			weaponId,
-			code,
-			name,
-			modifier
-		FROM
-			".DB_PREFIX."_Weapons
-		WHERE
-			game='".mysql_escape_string($gamecode)."'
-		ORDER BY
-			code ASC
-	");
-
-	$edlist->draw($query);
-?>
-
-<table width="75%" border="0" cellspacing="0" cellpadding="0">
-<tr>
-	<td align="center"><input type="submit" value=" <?php echo l('Apply'); ?> " class="submit"></td>
-</tr>
-</table>
+<div id="sidebar">
+	<h1><?php echo l('Options'); ?></h1>
+	<div class="left-box">
+		<ul class="sidemenu">
+			<li>
+				<a href="index.php?mode=admin&task=gameoverview&code=<?php echo $gc; ?>"><?php echo l('Back to game overview'); ?></a>
+			</li>
+			<li>
+				<a href="index.php?mode=admin"><?php echo l('Back to admin overview'); ?></a>
+			</li>
+		</ul>
+	</div>
+</div>
+<div id="main">
+	<h1><?php echo l('Actions for '); ?>: <?php echo $servers[0]['gameName']; ?></h1>
+	<p>
+		<?php echo l('You can give each weapon a "points modifier", amultiplier which determines how many points will be gained or lost for killing with or being killed by that weapon'); ?>.
+		<?php echo l('(Refer to'), ' <a href="index.php?mode=help#points">', l('Help'),'</a> ' ?>
+		<?php echo l("for a full description of how points ratings are calculated.) The baseline points modifier for weapons is 1.00. A points modifier of 0.00 will cause kills with that weapon to have no effect on players' points"); ?>.
+	</p>
+	<?php
+		if(!empty($return)) {
+			if($return['status'] === "1") {
+				echo '<div class="error">',$return['msg'],'</div>';
+			}
+			elseif($return['status'] === "2") {
+				echo '<div class="success">',$return['msg'],'</div>';
+			}
+		}
+	?>
+	<a name="teams"></a>
+	<form method="post" action="">
+		<table cellpadding="2" cellspacing="0" border="0" width="100%">
+			<tr>
+				<th><?php echo l('Weapon Code'); ?></th>
+				<th><?php echo l('Weapon Name'); ?></th>
+				<th><?php echo l('Points Modifier'); ?></th>
+				<th><?php echo l('Delete'); ?></th>
+			</tr>
+		<?php if(!empty($weapons)) {
+			foreach($weapons as $w) {
+		?>
+			<tr>
+				<td class="<?php echo toggleRowClass($rcol); ?>">
+					<input type="text" name="code[<?php echo $w['weaponId']; ?>]" value="<?php echo $w['code']; ?>" />
+				</td>
+				<td class="<?php echo $rcol; ?>">
+					<input type="text" name="name[<?php echo $w['weaponId']; ?>]" value="<?php echo $w['name']; ?>" />
+				</td>
+				<td class="<?php echo $rcol; ?>">
+					<input type="text" size="5" name="modifier[<?php echo $w['weaponId']; ?>]" value="<?php echo $w['modifier']; ?>" />
+				</td>
+				<td class="<?php echo $rcol; ?>">
+					<input type="checkbox" name="del[<?php echo $w['weaponId']; ?>]" value="1" />
+				</td>
+			</tr>
+		<?php
+			}
+		}
+		?>
+			<tr>
+				<td class="<?php echo toggleRowClass($rcol); ?>">
+					<?php echo l('new'); ?> <input type="text" name="newcode" value="" />
+				</td>
+				<td class="<?php echo $rcol; ?>">
+					<input type="text" name="newname" value="" />
+				</td>
+				<td colspan="2" class="<?php echo $rcol; ?>">
+					<input type="text" name="newmodifier" value="" />
+				</td>
+			</tr>
+			<tr>
+				<td colspan="4" align="right">
+					<button type="submit" name="sub[saveWeapon]" title="<?php echo l('Save'); ?>">
+						<?php echo l('Save'); ?>
+					</button>
+				</td>
+			</tr>
+		</table>
+	</form>
+</div>
