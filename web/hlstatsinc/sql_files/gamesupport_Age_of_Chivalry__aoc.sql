2d7bb70340c7820f66f69a9957c9c7a12efe2e9d
Banana <banana@starscream>
2011-05-27 12:47:15 +0200

    zps updates

--- web/hlstatsinc/sql_files/gamesupport_Age_of_Chivalry__aoc.sql
+++ web/hlstatsinc/sql_files/gamesupport_Age_of_Chivalry__aoc.sql
@@ -16,7 +16,38 @@ INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('aoc','Age of Chivalry','1','0'
 #
 # Awards
 #
-
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Flamberge', 'Flamberge', 'kills with Flamberge');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Longsword', 'Longsword', 'kills with Longsword');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Glaive', 'Glaive', 'kills with Glaive');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Dual Daggers', 'Dual Daggers', 'kills with Dual Daggers');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Flamberge & Kite Shield', 'Flamberge & Kite Shield', 'kills with Flamberge & Kite Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Shortsword', 'Shortsword', 'kills with Shortsword');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Warhammer', 'Warhammer', 'kills with Warhammer');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Mace', 'Mace', 'kills with Mace');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Mace & Buckler', 'Mace & Buckler', 'kills with Mace & Buckler');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Broadsword & Evil Shield', 'Mason Broadsword & Shield', 'kills with Mason Broadsword & Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Crossbow', 'Crossbow', 'kills with Crossbow');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Longbow', 'Longbow', 'kills with Longbow');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Longsword & Kite Shield', 'Longsword & Kite Shield', 'kills with Longsword & Kite Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Broadsword & Good Shield', 'Knights Broadsword & Shield', 'kills with Knights Broadsword & Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Hatchet', 'Hatchet', 'kills with Hatchet');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Double Axe', 'Battle Axe', 'kills with Battle Axe');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Flail & Evil Shield', 'Mason Flail & Shield', 'kills with Mason Flail & Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Flail & Good Shield', 'Knights Flail & Shield', 'kills with Knights Flail & Shield');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Javelin', 'Javelin', 'kills with Javelin');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Spiked Mace', 'Spiked Mace', 'kills with Spiked Mace');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Spear & Buckler', 'Spear & Buckler', 'kills with Spear & Buckler');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Spiked Mace & Buckler', 'Spiked Mace & Buckler', 'kills with Spiked Mace & Buckler');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Dagger', 'Dagger', 'kills with Dagger');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Broadsword', 'Broadsword', 'kills with Broadsword');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Throwing Knife', 'Throwing Knife', 'kills with Throwing Knives');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Halberd', 'Halberd', 'kills with Halberd');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'chivalry', 'Fire', 'kills with Fire');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Oil Pot', 'Oil Pot', 'kills with Oil Pot');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('O', 'aoc', 'headshot', 'Headshot/Decapitation', 'Headshots and Decapitations');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc','latency','Best Latency','ms average connection');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Fists','Fists','kills with fists');
+INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'aoc', 'Throwing Axe','Throwing Axe','kills with throwing axes');
 
 
 #
