f81d0f2b5704afd3a262ef496db96078eab97cef
Banana <banana@starscream>
2011-05-27 14:49:50 +0200

    l4d2 actions and awards

--- web/hlstatsinc/sql_files/gamesupport_Left_4_Dead_2__l4d2.sql
+++ web/hlstatsinc/sql_files/gamesupport_Left_4_Dead_2__l4d2.sql
@@ -90,6 +90,38 @@ INSERT IGNORE INTO `++DB_PREFIX++_Awards` VALUES ('W', 'l4d2', 'rifle_m60', 'M60
 #
 # Actions
 #
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'headshot', 0, 0, '', 'Headshot Kill', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_smoker', 1, 0, '', 'Killed a Smoker', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_boomer', 1, 0, '', 'Killed a Boomer', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_hunter', 1, 0, '', 'Killed a Hunter', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_tank', 3, 0, '', 'Killed a Tank', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_witch', 3, 0, '', 'Killed a Witch', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_spitter', 3, 0, '', 'Killed a Spitter', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_jockey', 3, 0, '', 'Killed a Jockey', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_charger', 3, 0, '', 'Killed a Charger', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_survivor', 25, 0, '', 'Incapacitated/Killed Survivor', '', '1', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'tongue_grab', 6, 0, '', '(Smoker) Tongue Grabbed Survivor', '', '1', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'scavenge_win', 0, 5, '', 'Scavenge Team Win', '', '', '1', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'versus_win', 0, 5, '', 'Versus Team Win', '', '', '1', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'defibrillated_teammate', 5, 0, '', 'Defibrillated Teammate', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'used_adrenaline', 0, 0, '', 'Used Adrenaline', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'jockey_ride', 5, 0, '', 'Going for a ride', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'charger_pummel', 5, 0, '', 'Charger Pummeling', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'bilebomb_tank', 5, 0, '', 'Tank Bilebombed', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'spitter_acidbath', 5, 0, '', 'Spitter Acid', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'rescued_survivor', 2, 0, '', 'Rescued Teammate', '1', '0', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'healed_teammate', 5, 0, '', 'Healed Teammate', '1', '0', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'revived_teammate', 3, 0, '', 'Revived Teammate', '1', '0', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'startled_witch', -5, 0, '', 'Startled the Witch', '1', '0', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'pounce', 6, 0, '', '(Hunter) Pounced on Survivor', '0', '1', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'vomit', 6, 0, '', '(Boomer) Vomited on Survivor', '0', '1', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'friendly_fire', -3, 0, '', 'Friendly Fire', '1', '0', '0', '0');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'cr0wned', 0, 0, '', 'Cr0wned (killed witch with single headshot)', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'hunter_punter', 0, 0, '', 'Hunter Punter (melee a Hunter mid-jump)', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'tounge_twister', 0, 0, '', 'Tounge Twister (kill a Smoker while he is dragging you)', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'protect_teammate', 0, 0, '', 'Protected Teammate', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'no_death_on_tank', 0, 0, '', 'No survivors died/incapped from tank', '1', '', '', '');
+INSERT IGNORE INTO `++DB_PREFIX++_Actions` VALUES ('l4d2', 'killed_all_survivors', 0, 0, '', 'Killed all survivors', '1', '', '', '');
 
 #
 # Teams
