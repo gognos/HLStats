6f833e882243147db2412d02f0dc05ecd0694db4
Banana <banana@starscream>
2011-05-27 12:53:21 +0200

    sql updates since sime where broken...sorry about that

--- web/hlstatsinc/sql_files/gamesupport_Age_of_Chivalry__aoc.sql
+++ web/hlstatsinc/sql_files/gamesupport_Age_of_Chivalry__aoc.sql
@@ -73,38 +73,37 @@ INSERT IGNORE INTO `++DB_PREFIX++_Team` VALUES (NULL,'aoc','Spectator','Spectato
 #
 # Weapons
 #
-INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES
-(NULL,'aoc', 'flamberge','Flamberge',1),
-(NULL,'aoc', 'longsword','Longsword',1),
-(NULL,'aoc', 'halberd','Knights Halberd',1),
-(NULL,'aoc', 'dagger','Dual Daggers',1),
-(NULL,'aoc', 'flamberge_kiteshield', 'Flamberge & Kite Shield', 1),
-(NULL,'aoc', 'world','World',1),
-(NULL,'aoc', 'chivalry','Chivalry',1),
-(NULL,'aoc', 'sword2','Shortsword',1),
-(NULL,'aoc', 'warhammer','Warhammer',1),
-(NULL,'aoc', 'mace','Mace',1),
-(NULL,'aoc', 'mace_buckler','Mace & Buckler',1),
-(NULL,'aoc', 'sword01_evil_shield', 'Mason Broadsword & Shield', 1),
-(NULL,'aoc', 'crossbow','Crossbow',1),
-(NULL,'aoc', 'longbow','Longbow',1),
-(NULL,'aoc', 'longsword_kiteshield', 'Longsword & Kite Shield', 1),
-(NULL,'aoc', 'sword01_good_shield', 'Knights Broadsword & Shield', 1),
-(NULL,'aoc', 'onehandaxe', 'Hatchet', 1),
-(NULL,'aoc', 'doubleaxe','Battle Axe',1),
-(NULL,'aoc', 'flail_evil_shield','Mason Flail & Shield',1),
-(NULL,'aoc', 'flail_good_shield','Knights Flail & Shield',1),
-(NULL,'aoc', 'thrown_spear', 'Javelin', 1),
-(NULL,'aoc', 'spear_buckler', 'Knights Spear & Buckler', 1),
-(NULL,'aoc', 'dagger2', 'Dagger', 1),
-(NULL,'aoc', 'mtest', 'Footman Longsword', 1),
-(NULL,'aoc', 'thrown_dagger2', 'Thrown Dagger', 1),
-(NULL,'aoc', 'spear_buckler2', 'Mason Spear & Buckler', 1),
-(NULL,'aoc', 'shortsword', 'Spiked Mace', 1),
-(NULL,'aoc', 'spikedmace_buckler','Spiked Mace & Buckler',1),
-(NULL,'aoc', 'evil_halberd', 'Mason Halberd', 1),
-(NULL,'aoc', 'env_explosion', 'Fire', 1, 4, 0),
-(NULL,'aoc', 'oilpot', 'Oil Pot', 1, 1, 0);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'flamberge','Flamberge',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'longsword','Longsword',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'halberd','Knights Halberd',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'dagger','Dual Daggers',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'flamberge_kiteshield', 'Flamberge & Kite Shield', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'world','World',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'chivalry','Chivalry',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'sword2','Shortsword',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'warhammer','Warhammer',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'mace','Mace',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'mace_buckler','Mace & Buckler',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'sword01_evil_shield', 'Mason Broadsword & Shield', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'crossbow','Crossbow',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'longbow','Longbow',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'longsword_kiteshield', 'Longsword & Kite Shield', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'sword01_good_shield', 'Knights Broadsword & Shield', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'onehandaxe', 'Hatchet', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'doubleaxe','Battle Axe',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'flail_evil_shield','Mason Flail & Shield',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'flail_good_shield','Knights Flail & Shield',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'thrown_spear', 'Javelin', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'spear_buckler', 'Knights Spear & Buckler', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'dagger2', 'Dagger', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'mtest', 'Footman Longsword', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'thrown_dagger2', 'Thrown Dagger', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'spear_buckler2', 'Mason Spear & Buckler', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'shortsword', 'Spiked Mace', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'spikedmace_buckler','Spiked Mace & Buckler',1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'evil_halberd', 'Mason Halberd', 1);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'env_explosion', 'Fire', 1, 4, 0);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'aoc', 'oilpot', 'Oil Pot', 1, 1, 0);
 
 
 # end of file
--- web/hlstatsinc/sql_files/gamesupport_The_Hidden_Source__hidden.sql
+++ web/hlstatsinc/sql_files/gamesupport_The_Hidden_Source__hidden.sql
@@ -15,7 +15,16 @@ INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('hidden','The Hidden: Source','
 #
 # Awards
 #
-
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'fn2000', 'FN2000 Assault Rifle', 'kills with FN2000 Assault Rifle');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'p90', 'FN P90 Sub Machine Gun', 'kills with FN P90 Sub Machine Gun');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'shotgun', 'Remington 870 MCS Shotgun', 'kills with Remington 870 MCS Shotgun');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'fn303', 'FN303 Less Lethal Launcher', 'kills with FN303 Less Lethal Launcher');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'pistol', 'FN FiveSeven Pistol', 'kills with FN FiveSeven Pistol');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'pistol2', 'FNP-9 Pistol', 'kills with FNP-9 Pistol');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'knife', 'Kabar D2 Knife', 'kills with Kabar D2 Knife');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'grenade_projectile', 'Pipe Bomb', 'kills with Pipe Bomb');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden', 'physics', 'Physics', 'kills with Physics');
+INSERT IGNORE INTO `++DB_PREFIX++_Games` VALUES ('W', 'hidden','latency','Best Latency','ms average connection');
 
 #
 # Player Actions
@@ -40,16 +49,15 @@ INSERT IGNORE INTO `++DB_PREFIX++_Teams` VALUES (NULL,'hidden','Spectator','Spec
 # Weapons
 #
 
-INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES
-(NULL,'hidden', 'fn2000','FN2000 Assault Rifle',1.50),
-(NULL,'hidden', 'p90','FN P90 Sub Machine Gun',2.00),
-(NULL,'hidden', 'shotgun','Remington 870 MCS Shotgun',2.00),
-(NULL,'hidden', 'fn303','FN303 Less Lethal Launcher',2.00),
-(NULL,'hidden', 'pistol','FN FiveSeven Pistol',3.00),
-(NULL,'hidden', 'pistol2','FNP-9 Pistol',3.00),
-(NULL,'hidden', 'knife','Kabar D2 Knife',2.50),
-(NULL,'hidden', 'grenade_projectile','Pipe Bomb',2.00),
-(NULL,'hidden', 'physics','Physics',3.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'fn2000','FN2000 Assault Rifle',1.50);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'p90','FN P90 Sub Machine Gun',2.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'shotgun','Remington 870 MCS Shotgun',2.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'fn303','FN303 Less Lethal Launcher',2.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'pistol','FN FiveSeven Pistol',3.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'pistol2','FNP-9 Pistol',3.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'knife','Kabar D2 Knife',2.50);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'grenade_projectile','Pipe Bomb',2.00);
+INSERT IGNORE INTO `++DB_PREFIX++_Weapons` VALUES (NULL,'hidden', 'physics','Physics',3.00);
 
 
 # end of file
