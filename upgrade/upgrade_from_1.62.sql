14c0d285ab867a3c36f082c6a1b686afc17679cb
Banana <banana@starscream>
2012-07-24 15:21:09 +0200

    missing table statement

--- upgrade/upgrade_from_1.62.sql
+++ upgrade/upgrade_from_1.62.sql
@@ -19,3 +19,20 @@ INSERT INTO `#DB_PREFIX#_Server_Addons` (`rule`,`addon`,`url`) VALUES ('smm_vers
 INSERT INTO `#DB_PREFIX#_Server_Addons` (`rule`,`addon`,`url`) VALUES ('spatialstats_version','Spatial statistics logging %','http://addons.eventscripts.com/addons/view/spatialstats');
 INSERT INTO `#DB_PREFIX#_Server_Addons` (`rule`,`addon`,`url`) VALUES ('eventscripts_ver','EventScripts','http://www.eventscripts.com/');
 INSERT INTO `#DB_PREFIX#_Server_Addons` (`rule`,`addon`,`url`) VALUES ('cssmatch_version','CSSMatch Plugin %','http://www.cssmatch.com/');
+
+DROP TABLE IF EXISTS `#DB_PREFIX#_Events_PlayerAttackedPlayer`;
+CREATE TABLE `#DB_PREFIX#_Events_PlayerAttackedPlayer` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `eventTime` datetime DEFAULT NULL,
+		  `serverId` int(10) DEFAULT NULL,
+		  `map` varchar(64) DEFAULT NULL,
+		  `playerId` int(10) DEFAULT NULL,
+		  `weapon` varchar(64) DEFAULT NULL,
+		  `victimId` int(10) DEFAULT NULL,
+		  `damage` int(10) DEFAULT NULL,
+		  `armor` int(10) DEFAULT NULL,
+		  `health` int(10) DEFAULT NULL,
+		  `hitgroup` varchar(32) DEFAULT NULL,
+		  `damage_armor` int(10) DEFAULT NULL,
+		  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
